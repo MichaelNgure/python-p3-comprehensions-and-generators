822f61e29a6998ae8da08694f144fb5f2fdbd5b0
MichaelNgure <mikynjoro01@gmail.com>
2024-08-29 09:38:55 +0300

    comprehension and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [item for item in num_list if item % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    # !using for loop
+    # sentenceStrings = []
+
+    # for item in sentence_list:
+    #     sentenceStrings.append(f"{item}!")
+
+    # return sentenceStrings
+
+    # !using list comperehension
+    sentence_exclamation = [f'{item}!' for item in sentence_list]
+    return sentence_exclamation
